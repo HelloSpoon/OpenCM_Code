9da8b33b39d1697e4025cc5d7a4cfd8df17e8259
HelloSpoon embedded Dev <hellospoonpr@gmail.com>
2014-12-23 01:14:07 -0700

    Create HelloSpoon_Animator.ino

---
+++ HelloSpoon_Animator.ino
@@ -0,0 +1,16 @@
+/*
+Code used to animate HelloSpoon robot
+with HelloSpoon Animator Android app.
+*/
+
+#include <"HelloSpoon.h">
+
+HelloSpoon robot;
+
+void setup(){
+  robot.begin();
+}
+
+void loop(){
+
+}
