66888bf5a763cf79c4772b9f256b390ecb87809b
HelloSpoon embedded Dev <hellospoonpr@gmail.com>
2014-12-23 00:15:57 -0700

    Update HelloSpoon_FactoryCode.ino

--- HelloSpoon_FactoryCode.ino
+++ HelloSpoon_FactoryCode.ino
@@ -1,3 +1,24 @@
+/*
+HelloSpoon Factory Reset Code.
+
+This is code is loaded on the OpenCM-9.04 inside HelloSpoon robot.
+
+If for any reason you download a different code 
+and want to return your HelloSpoon as its factory state
+you MUST load this code using CM9 IDE or HelloSpoon IDE.
+
+For more information about HelloSpoon IDE:
+https://github.com/HelloSpoon/HelloSpoon_OpenCMLib
+
+Trunk design:
+ 
+ID 1: XL-320 2.0 -------- base
+ID 2: XL-320 2.0 -------- strong elbow (right)
+ID 3: XL-320 2.0 -------- strong elbow (left)
+ID 4: XL-320 2.0 -------- elbow near spoon 
+ID 5: XL-320 2.0 -------- spoon 
+ */
+
 #include <HelloSpoon.h>
 
 HelloSpoon robot;
