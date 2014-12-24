4a295da9457c2ffa8428f203cbfb0a9b8a8c8f06
HelloSpoon embedded Dev <hellospoonpr@gmail.com>
2014-12-23 20:27:51 -0700

    Update HelloSpoon_Animator.ino

--- HelloSpoon_Animator.ino
+++ HelloSpoon_Animator.ino
@@ -2,7 +2,6 @@
 Code used to animate HelloSpoon robot
 with HelloSpoon Animator Android app.
 */
-
 #include <HelloSpoon.h>
 
 char data = 0;
@@ -34,9 +33,7 @@ void loop(){
     data = Serial2.read();
     if(data!=0){
       
-      if(data == 'a'){
-        activate = true;
-      }
+      if(data == 'a'){ activate = true; }
       
       else if(data == 'b'){
         activate = false;
@@ -73,49 +70,27 @@ void loop(){
         }
       }
       
-      else if(data == 'g'){
-        num_mot = 1;
-      }
+      else if(data == 'g'){ num_mot = 1; }
 
-      else if(data == 'h'){
-        num_mot = 2;
-      }
+      else if(data == 'h'){ num_mot = 2; }
       
-      else if(data == 'i'){
-        num_mot = 3;
-      }
+      else if(data == 'i'){ num_mot = 3; }
       
-      else if(data == 'j'){
-        num_mot = 4;
-      }
+      else if(data == 'j'){ num_mot = 4; }
       
-      else if(data == 'm'){
-        num_vel = 50;
-      }
+      else if(data == 'm'){ num_vel = 50; }
 
-      else if(data == 'n'){
-        num_vel = 200;
-      }
+      else if(data == 'n'){ num_vel = 200; }
       
-      else if(data == 'o'){
-        num_vel = 400;
-      }
+      else if(data == 'o'){ num_vel = 400; }
       
-      else if(data == 'p'){
-        num_vel = 600;
-      }
+      else if(data == 'p'){ num_vel = 600; }
       
-      else if(data == 'q'){
-        num_time = 3000;
-      }
+      else if(data == 'q'){ num_time = 3000; }
       
-      else if(data == 'r'){
-        num_time = 1000;
-      }
+      else if(data == 'r'){ num_time = 1000; }
       
-      else if(data == 's'){
-        num_time = 500;
-      }
+      else if(data == 's'){ num_time = 500; }
       
     }
   }
